bf823f9c64d774e319070c048b4d2101f23ceccc
EMAN-AYMAN22 <eman.aymanmos131@gmail.com>
2025-08-30 18:09:39 +0300

    Update MainLayout.jsx

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Component/footer";
+import Footer from "../Component/Footer";
 import Navbar from "../Component/Navbar";
 
 export default function MainLayout() {
